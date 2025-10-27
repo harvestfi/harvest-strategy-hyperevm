06d80f38fac3e4952b4c9c2f1da3d48607e2505c
CryptJS13 <cryptjs13@protonmail.com>
2025-10-27 12:50:26 +0100

    fix

--- contracts/strategies/morpho/MorphoStrategyMainnet_GLT_UETH.sol
+++ contracts/strategies/morpho/MorphoStrategyMainnet_GLT_UETH.sol
@@ -13,13 +13,13 @@ contract MorphoStrategyMainnet_GLT_UETH is MorphoStrategy {
   ) public initializer {
     address underlying = address(0xBe6727B535545C67d5cAa73dEa54865B92CF7907);
     address morphoVault = address(0x0571362ba5EA9784a97605f57483f865A37dBEAA);
-    address ueth = address(0xb88339CB7199b77E23DB6E890353E22632Ba630f);
+    address usdc = address(0xb88339CB7199b77E23DB6E890353E22632Ba630f);
     MorphoStrategy.initializeBaseStrategy(
       _storage,
       underlying,
       _vault,
       morphoVault,
-      ueth
+      usdc
     );
   }
 }
